0a0d65e496d0936cd2f017389451721616f1ba26
pinkyMan79 <48048748+pinkyMan79@users.noreply.github.com>
2023-06-04 11:14:34 +0300

    init forum

---
+++ forumservice/src/main/java/one/terenin/forumservice/ForumserviceApplication.java
@@ -0,0 +1,13 @@
+package one.terenin.forumservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumserviceApplication.class, args);
+    }
+
+}
